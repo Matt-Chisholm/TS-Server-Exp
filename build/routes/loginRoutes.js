e52ee34b765b0585bc5f862ba2438b788ab6946a
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-10-03 09:27:15 -0700

    login form

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -4,6 +4,18 @@ exports.router = void 0;
 const express_1 = require("express");
 const router = (0, express_1.Router)();
 exports.router = router;
-router.get('/', (req, res) => {
-    res.send('hello!');
+router.get('/login', (req, res) => {
+    res.send(`
+    <form method="POST">
+        <div>
+            <label>Email</label>
+            <input name="email" />
+        </div>
+        <div>
+            <label>Password</label>
+            <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
+    </form>
+    `);
 });
